d5b76a30e55780451a76d540d745d2fe4788254c
Ingo Speckens <doningo@ymail.com>
2023-09-24 08:22:54 +0200

    Add_shop

---
+++ sales-taxes/src/main/java/com/github/brudijoe/shop/Shop.java
@@ -0,0 +1,70 @@
+package com.github.brudijoe.shop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.brudijoe.item.Item;
+import com.github.brudijoe.item.ItemCategory;
+
+public class Shop {
+
+    private List<ItemCategory> categories;
+
+    public Shop() {
+        this.categories = new ArrayList<>();
+        initializeCategories();
+    }
+
+    private void initializeCategories() {
+        ItemCategory foodCategory = createFoodCategory();
+        ItemCategory medicineCategory = createMedicineCategory();
+        ItemCategory bookCategory = createBookCategory();
+        ItemCategory products = createUncategorizedProducts();
+
+        categories.add(foodCategory);
+        categories.add(medicineCategory);
+        categories.add(bookCategory);
+        categories.add(products);
+    }
+
+    private ItemCategory createFoodCategory() {
+        ItemCategory foodCategory = new ItemCategory("Food");
+        foodCategory.addItem(new Item("Chocolate Bar", 0.85, false, true));
+        foodCategory.addItem(new Item("Box of Chocolates", 10.00, true, true));
+        foodCategory.addItem(new Item("Box of Chocolates", 11.25, true, true));
+        return foodCategory;
+    }
+
+    private ItemCategory createMedicineCategory() {
+        ItemCategory medicineCategory = new ItemCategory("Medicine");
+        medicineCategory.addItem(new Item("Packet of Headache Pills", 9.75, false, true));
+        return medicineCategory;
+    }
+
+    private ItemCategory createBookCategory() {
+        ItemCategory bookCategory = new ItemCategory("Books");
+        bookCategory.addItem(new Item("Book", 12.49, false, true));
+        return bookCategory;
+    }
+
+    private ItemCategory createUncategorizedProducts() {
+        ItemCategory products = new ItemCategory("Uncategorized Products");
+        products.addItem(new Item("Music CD", 14.99, false, false));
+        products.addItem(new Item("Bottle of Perfume", 27.99, true, false));
+        products.addItem(new Item("Bottle of Perfume", 47.50, true, false));
+        return products;
+    }
+
+    public List<String> getCategoryNames() {
+        List<String> categoryNames = new ArrayList<>();
+        for (int i = 0; i < categories.size(); i++) {
+            String categoryName = categories.get(i).getCategoryName();
+            categoryNames.add("(" + (i + 1) + ") " + categoryName);
+        }
+        return categoryNames;
+    }
+
+    public List<Item> getItemsInCategory(int categoryIndex) {
+        return categories.get(categoryIndex).getItems();
+    }
+}
