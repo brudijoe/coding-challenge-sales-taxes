b43964ad210de4a4a5a3b6c57adbdbfa255a4bf6
Ingo Speckens <doningo@ymail.com>
2023-10-04 19:44:12 +0200

    Cleanup

--- sales-taxes/src/main/java/com/github/brudijoe/item/Item.java
+++ sales-taxes/src/main/java/com/github/brudijoe/item/Item.java
@@ -19,33 +19,16 @@ public class Item {
         return this.name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public BigDecimal getPrice() {
         return this.price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
     }
 
     public int getQuantity() {
         return this.quantity;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-
     public int getTaxRate() {
         return this.taxRate;
-    }
-
-    public void setTaxRate(int taxRate) {
-        this.taxRate = taxRate;
     }
 
     @Override
